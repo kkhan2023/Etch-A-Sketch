5f3ea501b83a1f863d74c10268a179e6019849c7
kkhan2023 <kasamkhan@hotmail.co.uk>
2025-02-05 21:53:55 +0000

    Added 2 new features (RGB & FADE buttons). Additionally added UI and background image using only JS and CSS

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Etch-A-Sketch
 
-Project 3: Etch-A-Sketch
+Project 3: Etch-A-Sketch with UI
 
 Objective: Create Etch-A-Sketch game using primarily CSS & JavaScript. Starting off with a 16x16 grid and allow user input to change size of grid from values 1-100.
 
@@ -9,3 +9,9 @@ Learning outcome:
 - Created function with loop to generate divs into container via JS
 - Used mouseover event listener to make grids change colour on mouse hovering.
 - Used button click event listener to generate a user prompt so a value can be entered to change the grid size.
+
+Additional outcomes:
+
+- Added UI completely through JavaScript and CSS
+- Additionally, implemented a progressive darkening effect where each interaction darkens the square by 10%. The goal was to achieve a fully black (or completely colored) square in only ten interactions.
+- Added feature to randomize the squares’ RGB values with each interaction.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createGrid(size) {
   const container = document.getElementById("container"); // get container element from DOM
   container.innerHTML = "";
 
-  let gridSize = 560 / size; // number of pixels divided by number of grids
+  let gridSize = 440 / size; // number of pixels divided by number of grids
 
   for (let i = 0; i < size * size; i++) {
     // create loop for div generation
@@ -12,7 +12,6 @@ function createGrid(size) {
     div.classList.add("grid");
     div.style.width = `${gridSize}px`; // assign width to grid size
     div.style.height = `${gridSize}px`; // assign height to grid size
-    div.style.border = "solid black 1px";
     container.appendChild(div);
   }
   addHoverEffect(); // Initiates the hover effect for 16x16 grid & when the user enters different grid size. If this function was not called here, then hover effect would not work for other grid sizes chosen by user.
@@ -33,7 +32,7 @@ function addHoverEffect() {
         // reset the colour after a short delay
         setTimeout(() => {
           event.target.style.background = "";
-        }, 8000);
+        }, 20000);
       },
       false
     );
@@ -71,4 +70,131 @@ button.addEventListener("click", () => {
   }
 
   createGrid(userInput); // calls the createGrid function to generate another grid size based on user input
+});
+
+// add button to don for the RGB colour effect
+
+const buttonContainer = document.getElementById("buttonDiv");
+const changeToRgb = document.createElement("button");
+changeToRgb.innerHTML = "Change colour to rainbow";
+changeToRgb.setAttribute("id", "changeToRgb");
+buttonContainer.appendChild(changeToRgb);
+
+// Function to return a random array of colours for the changeRgbButton
+
+function rgb(num) {
+  const randomColours = ["#E40303", "#008026", "#732982", "#0000FF"];
+  let rgb = Math.floor(Math.random() * num);
+  if (rgb === 0) {
+    return randomColours[0];
+  } else if (rgb === 1) {
+    return randomColours[1];
+  } else if (rgb === 2) {
+    return randomColours[2];
+  } else {
+    return randomColours[3];
+  }
+}
+
+function addRgbHoverEffect() {
+  let divHover = document.querySelectorAll(".grid"); // query .grid class from DOM
+
+  divHover.forEach((div) => {
+    // As their are multiple div elements with the class .grid , an event listener must be attached to each .grid div.
+
+    div.addEventListener(
+      "mouseenter",
+      (event) => {
+        // highlight the mouseenter target
+        event.target.style.background = rgb(4);
+
+        // reset the colour after a short delay
+        setTimeout(() => {
+          event.target.style.background = "";
+        }, 20000);
+      },
+      false
+    );
+  });
+}
+
+changeToRgb.addEventListener("click", () => {
+  return addRgbHoverEffect();
+});
+
+// add button to change the squares opacity
+
+const changeToFade = document.createElement("button");
+changeToFade.innerHTML = "Change colour to fade";
+changeToFade.setAttribute("id", "changeToFade");
+buttonContainer.appendChild(changeToFade);
+
+// function to change EACH squares opacity in ten interactions progressively
+
+function addFadeHoverEffect() {
+  let divHover = document.querySelectorAll(".grid"); // query .grid class from DOM
+
+  divHover.forEach((div) => {
+    // As their are multiple div elements with the class .grid , an event listener must be attached to each .grid div.
+    let startOpacity = 0;
+    const targetOpacity = 1;
+
+    div.addEventListener(
+      "mouseenter",
+      (event) => {
+        if (startOpacity <= targetOpacity) {
+          startOpacity += 0.1;
+          event.target.style.opacity = startOpacity;
+          event.target.style.background = "black";
+        }
+        // highlight the mouseenter target
+
+        // reset the colour after a short delay
+        setTimeout(() => {
+          event.target.style.background = "";
+        }, 20000);
+      },
+      false
+    );
+  });
+}
+
+changeToFade.addEventListener("click", () => {
+  return addFadeHoverEffect();
+});
+
+// created container to wrap around child elements for styling
+
+const uiContainer = document.createElement("div");
+uiContainer.setAttribute("id", "uiContainer");
+const parent = buttonDiv.parentNode;
+parent.replaceChild(uiContainer, buttonDiv);
+uiContainer.appendChild(buttonDiv);
+uiContainer.appendChild(container);
+
+// Created new div to add to bottom of UI container to add 3 other elements
+
+const uiBottom = document.createElement("div");
+uiBottom.setAttribute("id", "uiBottom");
+uiContainer.appendChild(uiBottom);
+
+// Added left circle, reset button & right circle within uiBottom div.
+
+const leftCircle = document.createElement("span");
+uiBottom.appendChild(leftCircle);
+leftCircle.classList.add("circle");
+
+const resetButton = document.createElement("button");
+resetButton.setAttribute("id", "resetButton");
+resetButton.innerHTML = "Reset the game";
+uiBottom.appendChild(resetButton);
+
+const rightCircle = document.createElement("span");
+uiBottom.appendChild(rightCircle);
+rightCircle.classList.add("circle");
+
+// reset button for game
+
+resetButton.addEventListener("click", () => {
+  return createGrid(16);
 });
